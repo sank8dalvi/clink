c3fe5e44cff5a5c8626ab7432631380be2a7702e
HOD101s <manasbass.99@gmail.com>
2019-03-11 12:26:16 +0530

    removed classes

--- case/app.py
+++ case/app.py
@@ -15,7 +15,7 @@ db = {
 }
 
 try:
-	con = Connect.clinkConnect(db)
+	con = Connect.clinkConnect(**db)
 	cur = con.cursor()
 	print("Connected")
 except:
@@ -24,50 +24,47 @@ except:
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-class Departure:
-	global passDb, bagDb
 
-	@app.route('/dept/gen/pid' , methods =['GET'])
-	def genUid(self):
-		uid = uuid.uuid1()
-		passDb = str(uuid.uuid3(uid,"caseLink").hex)
-		passRfid = passDb								#requires hashing
-		render_template('.html' , passId = passRfid )	#passing passenger rfid to html
-		return passRfid
+''''DEPARTURE'''
+global passDb, bagDb
+@app.route('/dept/gen/pid' , methods =['GET'])
+def genUid():
+	uid = uuid.uuid1()
+	passDb = str(uuid.uuid3(uid,"caseLink").hex)
+	passRfid = passDb								#requires hashing
+	# render_template('.html' , passId = passRfid )	#passing passenger rfid to html
+	return passRfid
 
-	@app.route('/dept/gen/bid' , methods =['GET'])
-	def genBagid(self):
-		uid = uuid.uuid1()
-		bagDb = str(uuid.uuid3(uid, "caseLink").hex)
-		bagRfid = bagDb  								# requires hashing
-		render_template('.html', bagID = bagRfid)  		# passing bag rfid to html
-		return bagRfid
+@app.route('/dept/gen/bid' , methods =['GET'])
+def genBagid():
+	uid = uuid.uuid1()
+	bagDb = str(uuid.uuid3(uid, "caseLink").hex)
+	bagRfid = bagDb  								# requires hashing
+	# render_template('.html', bagID = bagRfid)  		# passing bag rfid to html
+	return bagRfid
 
-	@app.route('/dept/post/bagwt', methods = ["POST"])
-	def postBagWT(self):
-		wt = request.form['weight']						#HTML input name="weight"
-		cur.execute(Query.addpassbags.format(passDb, bagDb, wt))				#uploads passenger to databse
+@app.route('/dept/post/bagwt', methods = ["POST"])
+def postBagWT():
+	wt = request.form['weight']						#HTML input name="weight"
+	cur.execute(Query.addpassbags.format(passDb, bagDb, wt))				#uploads passenger to databse
 
+''''ARRIVAL'''
+@app.route('/arr/match', methods = ["POST"])
+def match(passID,bagID):
+	cur.execute(Query.matchtag.format(passID,bagID))		#update after Hashing added
+	if cur.rowcount == 0 :
+		return False
+	else:
+		cur.execute(Query.delTag.format(passID, bagID))
+		cur.execute(Query.UpColl.format(passID, bagID))
+		return True
 
-class Arrival:
-	@app.route('/arr/match', methods = ["POST"])
-	def match(self,passID,bagID):
-		cur.execute(Query.matchtag.format(passID,bagID))		#update after Hashing added
-		if cur.rowcount == 0 :
-			return False
-		else:
-			cur.execute(Query.delTag.format(passID, bagID))
-			cur.execute(Query.UpColl.format(passID, bagID))
-			return True
-
-
-class UI:
-	''''Table'''
-	@app.route('/populateTable')
-	def popTab():
-		cur.execute(Query.gettable)
-		data = cur.fetchall()
-		render_template('.html', data = data)			#add passenger details html
+''''Table'''
+@app.route('/populateTable')
+def popTab():
+	cur.execute(Query.gettable)
+	data = cur.fetchall()
+	# render_template('.html', data = data)			#add passenger details html
 
 
 app.run()
