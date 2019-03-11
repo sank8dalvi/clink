97b936983fbfd8793a2d2a765ba70a8463bad056
HOD101s <manasbass.99@gmail.com>
2019-03-11 15:15:51 +0530

    added bagwt + query backquotes

--- case/model/query.py
+++ case/model/query.py
@@ -1,6 +1,6 @@
 class Query:
 	addpassbags = "INSERT INTO `tagdata` (`passID`, `bagID`, `bagWT`) VALUES ({},{},{})"
-	matchtag = "SELECT * FROM `tagdata` WHERE `passID` = `{}` AND `bagID` = `{}` LIMIT 1"
-	delTag = "DELETE FROM `tagdata` WHERE `{}` = 34545 AND `bagID` = `{}`"
+	matchtag = "SELECT * FROM `tagdata` WHERE `passID` = {} AND `bagID` = {} LIMIT 1"
+	delTag = "DELETE FROM `tagdata` WHERE {} = 34545 AND `bagID` = {}"
 	gettable = "SELECT * FROM `tagdata`"
-	UpColl = "INSERT INTO `collected`(`uid`, `passID`, `bagID`, `bagWT`) VALUES (`{}`,`{}`,`{}`)"
+	UpColl = "INSERT INTO `collected`(`uid`, `passID`, `bagID`, `bagWT`) VALUES ({},{},{})"
