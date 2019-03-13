9cd2783c41304f0dc9c88e7dfc20422dfc86b88a
Sahil Nirkhe <sahilnirkhe@outlook.com>
2019-03-13 18:03:29 +0530

    refractored and reformatted app.py

--- case/app.py
+++ case/app.py
@@ -7,30 +7,30 @@ from case.model.query import Query
 from flask_cors import CORS
 
 db = {
-    'user': 'root',
-    'password': '',
-    'host': '127.0.0.1',
-    'database': 'clink'
+	'user': 'root',
+	'password': '',
+	'host': '127.0.0.1',
+	'database': 'clink'
 }
 
 try:
-    con = clinkConnect(db)
-    cur = con.cursor()
-    print("Connected")
+	con = clinkConnect(db)
+	cur = con.cursor()
+	print("Connected")
 except:
-    print("Not Connected")
+	print("Not Connected")
 
 '''DASHBOARD Data'''
 
 
-def getPassCount():
-    cur.execute(Query.getPassCount)
-    return (cur.fetchall()[0][0])
+def get_pass_count():
+	cur.execute(Query.getPassCount)
+	return cur.fetchall()[0][0]
 
 
-def getBagCount():
-    cur.execute(Query.getBagCount)
-    return (cur.fetchall()[0][0])
+def get_bag_count():
+	cur.execute(Query.getBagCount)
+	return cur.fetchall()[0][0]
 
 
 app = flask.Flask(__name__)
@@ -40,57 +40,60 @@ CORS(app)
 
 @app.route('/')
 def homepage():
-    return render_template('home.html', bagCount=getBagCount(), passCount=getPassCount())
+	return render_template('home.html', bagCount=get_bag_count(), passCount=get_pass_count())
 
 
 ''''DEPARTURE'''
 
 
 @app.route('/dept/gen/pid', methods=['GET'])
-def genUid():
-    uid = uuid.uuid1()
-    passRfid = uuid.uuid3(uid, "caseLink")
-    passDb = str(uuid.uuid5(passRfid, "caseLink").hex)
-    passRfid = str(passRfid.hex)
-    return jsonify({'passRfid': passRfid, 'passDb': passDb})
+def gen_uid():
+	uid = uuid.uuid1()
+	passRfid = uuid.uuid3(uid, "caseLink")
+	passDb = str(uuid.uuid5(passRfid, "caseLink").hex)
+	passRfid = str(passRfid.hex)
+	return jsonify({'passRfid': passRfid, 'passDb': passDb})
 
 
-@app.route('/dept/gen/idwrite')  # write Rfid Page
-def callWriteRead():
-    return render_template('write-read.html', status="Please Scan Tag",
-                           bagCount=getBagCount(), passCount=getPassCount())
+# write Rfid Page
+@app.route('/dept/gen/idwrite')
+def call_write_read():
+	return render_template('write-read.html', status="Please Scan Tag",
+						   bagCount=get_bag_count(), passCount=get_pass_count())
 
 
-@app.route('/dept/gen/idread')  # read Rfid Page	add delay should try to use JS and modify status directly
-def pidWritten():
-    return render_template('write-read.html', status="Tag Scanned Successfully",
-                           bagCount=getBagCount(), passCount=getPassCount())
+# read Rfid Page	add delay should try to use JS and modify status directly
+@app.route('/dept/gen/idread')
+def pid_written():
+	return render_template('write-read.html',
+						   status="Tag Scanned Successfully",
+						   bagCount=get_bag_count(),
+						   passCount=get_pass_count())
 
 
 @app.route('/dept/gen/bid', methods=['GET'])
-def genBagid():
-    uid = uuid.uuid1()
-    bagRfid = uuid.uuid3(uid, "caseLink")
-    bagDb = str(uuid.uuid5(bagRfid, "caseLink").hex)
-    bagRfid = str(bagRfid.hex)
-    # render_template('.html', bagID = bagRfid, bagCount = getBagCount(), passCount = getPassCount)  		# passing bag rfid to html
-    return jsonify({'bagRfid': bagRfid, 'passDb': bagDb})
+def gen_bag_id():
+	uid = uuid.uuid1()
+	bagRfid = uuid.uuid3(uid, "caseLink")
+	bagDb = str(uuid.uuid5(bagRfid, "caseLink").hex)
+	bagRfid = str(bagRfid.hex)
+	# render_template('.html', bagID = bagRfid, bagCount = getBagCount(), passCount = getPassCount)
+	return jsonify({'bagRfid': bagRfid, 'passDb': bagDb})
 
 
 '''@todo check how values are passing. API needs passenger and Bag db IDs'''
 
 
 @app.route('/dept/post/bagwt', methods=["POST"])
-def postBagWT():
-    wt = request.form['weight']  # HTML input name="weight"
-    pdb = request.form['pdb']  # piInput
-    bdb = request.form['bdb']  # piInput
-    # bdb = request.args.get('bdb','')							#piInput
-    # cur.execute(Query.addpassbags.format(pdb, bdb, wt))				#uploads passenger to databse
-    print(Query.addpassbags.format(pdb, bdb, int(wt)))
-    cur.execute(Query.addpassbags.format(pdb, bdb, int(wt)))
-    con.commit()
-    return '1'
+def post_bag_wt():
+	wt = request.form['weight']
+	pdb = request.form['pdb']
+	bdb = request.form['bdb']
+
+	print(Query.addpassbags.format(pdb, bdb, int(wt)))
+	cur.execute(Query.addpassbags.format(pdb, bdb, int(wt)))
+	con.commit()
+	return '1'
 
 
 # return render_template('.html', bagCount = getBagCount(), passCount = getPassCount)
@@ -101,56 +104,61 @@ def postBagWT():
 
 @app.route('/arr/match', methods=["POST"])
 def match():
-    bagID = request.form['bagID']  # piInput
-    passID = request.form['passID']  # piInput
-    print(bagID, passID)
-    bagID = str(uuid.uuid5(uuid.UUID(bagID), "caseLink").hex)
-    passID = str(uuid.uuid5(uuid.UUID(passID), "caseLink").hex)
-    print(bagID, passID)
-    cur.execute(Query.matchtag.format(passID, bagID))
-    data = cur.fetchall()
-    resp = {}
-    if len(data) == 0:
-        resp['status'] = False
-    else:
-        # todo row count
-        print(data)
-        cur.execute(Query.delTag.format(passID, bagID))
-        cur.execute(Query.UpColl.format(passID, bagID, data[0][3]))
-        con.commit()
-        resp['status'] = True
+	# piInputs
+	bagID = request.form['bagID']
+	passID = request.form['passID']
+	print(bagID, passID)
+	bagID = str(uuid.uuid5(uuid.UUID(bagID), "caseLink").hex)
+	passID = str(uuid.uuid5(uuid.UUID(passID), "caseLink").hex)
+	print(bagID, passID)
+	cur.execute(Query.matchtag.format(passID, bagID))
+	data = cur.fetchall()
+	resp = {}
+	if len(data) == 0:
+		resp['status'] = False
+	else:
+		# todo row count
+		print(data)
+		cur.execute(Query.delTag.format(passID, bagID))
+		cur.execute(Query.UpColl.format(passID, bagID, data[0][3]))
+		con.commit()
+		resp['status'] = True
 
-    return jsonify(resp)
+	return jsonify(resp)
 
 
 ''''Table'''
 
 
 @app.route('/popBagTable')  # All bags from db
-def popBagTab():
-    passID = request.args.get('passID', '')  # piInput
-    cur.execute(Query.gettable.format(passID))
-    data = cur.fetchall()
-    return render_template('tags.html', data=data,
-                           bagCount=getBagCount(), passCount=getPassCount())  # add bag details html
+def pop_bag_tab():
+	passID = request.args.get('passID', '')  # piInput
+	cur.execute(Query.gettable.format(passID))
+	data = cur.fetchall()
+	return render_template('tags.html',
+						   data=data,
+						   bagCount=get_bag_count(),
+						   passCount=get_pass_count())  # add bag details html
 
 
 @app.route('/AddBags')  # Current passenger bags + on add bags page
-def popAddBagTab():
-    # passID = request.args.get('passID','')				#piInput
-    cur.execute(Query.getbags.format("12324"))
-    bags = cur.fetchall()
-    return render_template('addBags.html', data=bags,
-                           bagCount=getBagCount(), passCount=getPassCount())  # add passenger details html
+def pop_add_bag_tab():
+	# passID = request.args.get('passID','')				#piInput
+	cur.execute(Query.getbags.format("12324"))
+	bags = cur.fetchall()
+	return render_template('addBags.html',
+						   data=bags,
+						   bagCount=get_bag_count(),
+						   passCount=get_pass_count())  # add passenger details html
 
 
 ''''CLOSE CONN'''
 
 
 @app.route('/close')
-def conClose():
-    clinkClose(con)
-    return ("Closing")
+def con_close():
+	clinkClose(con)
+	return "Closing"
 
 
 app.run()
